18529adc2af4398d934260382161e03ff94433a6
aatango <80591203+aatango@users.noreply.github.com>
2022-04-10 21:44:37 +0200

    Generic classes

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,20 +3,39 @@
 
 #include <iostream>
 
-#include "../src/geom2d.hpp"
+#include "geom2d.hpp"
 
-TEST_SUITE("Point2d") {
-	auto point1 = geom2d::point2d(1, 2);
-	auto point2 = geom2d::point2d(4, 6);
+TEST_SUITE("Point2f") {
+	geom2::point2f point1(1, 2);
+	geom2::point2f point2(4, 6);
 
 	TEST_CASE("Definition") {
-		CHECK_EQ(point1.x, 1);
-		CHECK_EQ(point1.y, 2);
+		REQUIRE_EQ(point1.x, 1);
+		REQUIRE_EQ(typeid(point1.x), typeid(float));
+
+		REQUIRE_EQ(point1.y, 2);
+		REQUIRE_EQ(typeid(point1.y), typeid(float));
+
+	}
+
+	// To avoid duplication, further tests are made only on double precision objects.
+}
+
+TEST_SUITE("Point2d") {
+	geom2::point2d point1(1, 2);
+	geom2::point2d point2(4, 6);
+
+	TEST_CASE("Definition") {
+		REQUIRE_EQ(point1.x, 1);
+		REQUIRE_EQ(typeid(point1.x), typeid(double));
+		
+		REQUIRE_EQ(point1.y, 2);
+		REQUIRE_EQ(typeid(point1.y), typeid(double));
 	}
 
 	TEST_CASE("Operator overloads") {
 		CHECK_EQ(point1, point1);
-		CHECK_EQ(point1 + point2, geom2d::point2d(5, 8));
+		CHECK_EQ(point1 + point2, geom2::point2d(5, 8));
 	}
 
 	TEST_CASE("Methods") {
@@ -25,11 +44,15 @@ TEST_SUITE("Point2d") {
 }
 
 TEST_SUITE("Vector2d") {
-	auto vector1 = geom2d::vector2d(1, 2);
-	auto vector2 = geom2d::vector2d(4, 6);
+	geom2::vector2d vector1(1, 2);
+	geom2::vector2d vector2(4, 6);
 
 	TEST_CASE("Definition") {
-		CHECK_EQ(vector1.u, 1);
-		CHECK_EQ(vector1.v, 2);
+		REQUIRE_EQ(vector1.u, 1);
+		REQUIRE_EQ(typeid(vector1.u), typeid(double));
+		
+		REQUIRE_EQ(vector1.v, 2);
+		REQUIRE_EQ(typeid(vector1.v), typeid(double));
 	}
 }
+
